5818d234958c366694cf77c43f7e95f9e676ad30
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-03-16 11:31:23 +0530

    updates in gitignore filea

---
+++ STL/main.cpp
@@ -0,0 +1,14 @@
+#include "../bits/stdc++.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int t, n;
+    cin >> t;
+    cin >> n;
+    cout << "t is " << t << endl;
+    cout << "n is " << n;
+
+    return 0;
+}
