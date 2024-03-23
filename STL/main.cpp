9e2e4424c74c75d30b9ad4d650ea7e88a9ed5125
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-03-23 22:26:33 +0530

    C++ STL: Pairs explained

--- STL/main.cpp
+++ STL/main.cpp
@@ -2,13 +2,40 @@
 
 using namespace std;
 
+void print(int t, int n){
+    cout << "t is " << t << endl;
+    cout << "n is " << n;
+}
+
+void expainPair() { 
+    pair<int, int> p = {1, 3};
+
+    // pair
+    cout << p.first << " " << p.second << " ";
+
+    // pair of pairs
+    pair<int, pair<int, int>> pp = {1, {2, 3}};
+
+    cout << "first " << pp.first;
+    cout << "\nSecond " << pp.second.first << endl;
+
+    // pairs as array
+     pair<int, int> arr[] = { {1, 2}, {3, 4} };
+     cout << "First pair > second number " << arr[0].second;
+
+}
+
+
 int main(int argc, char const *argv[])
 {
     int t, n;
     cin >> t;
     cin >> n;
-    cout << "t is " << t << endl;
-    cout << "n is " << n;
+    // print(t, n);
+    expainPair();
 
     return 0;
 }
+ 
+
+
