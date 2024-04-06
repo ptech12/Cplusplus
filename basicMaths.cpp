d329641d2f2b92bfa0227c2dc7fde4e5ca52e4e0
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-04-06 23:08:18 +0530

    basicMaths.cpp: Reverse a Number

--- basicMaths.cpp
+++ basicMaths.cpp
@@ -3,11 +3,15 @@
 using namespace std;
 
 void countDigits();
+void revereseDigits();
+void checkPalindrome();
 
 int main(int argc, char const *argv[])
 {
-    countDigits();
-    
+    // countDigits();
+    revereseDigits();
+
+
     return 0;
 }
 
@@ -19,4 +23,38 @@ void countDigits(){
     cin >> n;
 
     cout << n << " => " << (int) log10(n) + 1;
+}
+
+void revereseDigits(){
+    int n, last;
+    int rev = 0; 
+    int flag=0;
+    cin >> n;
+    
+    cout << "Given: " << n << endl;
+    
+
+    // checking for N less than zero
+    if(n < 0){
+        n *= -1;
+        flag = 1;
+    }
+
+    while (n > 0)
+    {
+        // extract last digit
+        last = n % 10;
+        // add to rev digit
+        rev = (rev * 10) + last;
+        // remove last digit
+        n = n / 10;
+    }
+
+    if(flag){
+        rev *= -1;
+    }
+    cout << "Reverse: " << rev << endl;
+}
+
+void checkPalindrome(){
 }
