82bbe986dc0db88e26e2b6398302aaf288b6d449
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-04-14 20:12:51 +0530

    recursion: print name N to 1 and 1 to N using backtracking

--- recursion/start.cpp
+++ recursion/start.cpp
@@ -10,6 +10,28 @@ int print(int n){
 	return ( n==0 ) ? 0 : print(n-1);
 }
 
+void func(int i, int n);
+void linear(int i, int n);
+void reverse(int i, int n);
+void linearBacktrack(int i, int n);
+void reverseBacktrack(int i, int n);
+
+int main(){
+	int n;
+	cin >> n;
+	// print(n);
+	// linear(1, n);
+	// reverse(n, n);
+
+	// linearBacktrack(n, n);
+
+	reverseBacktrack(1, n);
+
+	return 0;
+}
+
+
+
 void func(int i, int n){
 	// TC => O(N)
 	// base case
@@ -18,14 +40,42 @@ void func(int i, int n){
 	cout <<  "ptech12" << endl;
 	// recursion
 	func(i+1, n);
-
 }
 
-int main(){
-	int n;
-	cin >> n;
-	// print(n);
-	func(1, n);
-	return 0;
+void linear(int i, int n)
+{
+	if(i > n)
+		return;
+	
+	cout << i << endl;
+	// recursion
+	linear(i + 1, n);
 }
 
+void reverse(int i, int n){
+	if(i < 1)
+		return;
+
+	cout << i << endl;
+	// recursion
+	reverse(i-1, n);
+}
+
+void linearBacktrack(int i, int n)
+{
+	if(i < 1)
+		return;
+	// backtrack
+	linearBacktrack(i - 1, n);
+	cout << i << endl;
+}
+
+void reverseBacktrack(int i, int n)
+{
+	if(n < i)
+		return;
+	// backtrack
+	reverseBacktrack(i+1, n);
+
+	cout << i << endl;
+}
