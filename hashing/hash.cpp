a5902f26afd8093bf7bb8b691c60f7d969ce004c
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-04-15 23:03:52 +0530

    hashing: hash using map and unordered map

--- hashing/hash.cpp
+++ hashing/hash.cpp
@@ -2,7 +2,55 @@
 
 using namespace std;
 
+
+/**
+ * * int input
+5
+1 3 2 1 3
+5
+1
+4
+2
+3
+12
+*/
+void intHashing();
+
+/**
+ * * char input
+abcdabehf
+5 
+a
+g
+h
+b
+c 
+*/
+void charHashing();
+
+/**
+ * Hash using Map
+ * input
+ * 
+7
+1 2 3 1 3 2 12
+5
+1
+2
+3
+4
+12
+ *
+*/
+void hashMap();
+
 int main(){
+   hashMap();
+    return 0;
+}
+
+void intHashing()
+{
     int n; cin >> n;
     int a[n];
     for(int i=0;i < n; i++){
@@ -34,5 +82,68 @@ int main(){
         // fetching
         cout << number << " appears: " << hash[number] << endl;
     }
-    return 0;
+}
+
+void charHashing()
+{
+    string s;
+    cin >> s;
+    int q;
+    cin >> q;
+    
+    int hash[26] = {0};
+    // pre-compute hash array
+    for(int i = 0; i < s.size(); i++){
+        // char - 'a'
+        hash[s[i] - 'a']++;
+    }
+    cout << s << endl;
+
+    while(q--){
+        char c; cin >> c;
+        // fetch
+        cout << c << ": " <<
+         hash[c - 'a'] << endl;
+    }
+}
+
+void hashMap()
+{
+    int n;
+    cin >> n;
+    int a[n];
+
+    for(int i=0;i<n;i++)
+        cin >> a[i];
+
+    // pre-compute
+    map<int , int> mpp;
+    for(int i=0; i < n; i++){
+        mpp[a[i]]++;
+    }
+    cout << "Given array: "; 
+    for(int i=0;i<n;i++)
+        cout << a[i] << " ";
+
+    cout << "\nhash map" << endl;
+    // iterate over the map
+    for(auto it : mpp){
+        cout << it.first << " -> " << it.second << endl;
+    }
+    cout << "Output" << endl;
+ 
+
+    int q;
+    
+    cin >> q;
+    
+    while(q--){
+        int num;
+        cin >> num;
+        // fetch
+        cout << mpp[num] << endl;
+    }
+
+
+
 }
---
+++ hashing/high_low.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    // for loops
+    
+    return 0;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ *  Problem statement
+
+Given an array 'v' of 'n' numbers.
+
+
+Your task is to find and return the highest and lowest frequency elements.
+
+
+If there are multiple elements that have the highest frequency or lowest frequency, pick the smallest element.
+
+
+Example:
+
+Input: ‘n' = 6, 'v' = [1, 2, 3, 1, 1, 4]
+
+Output: 1 2
+
+Explanation: The element having the highest frequency is '1', and the frequency is 3. The elements with the lowest frequencies are '2', '3', and '4'. Since we need to pick the smallest element, we pick '2'. Hence we return [1, 2].
+
+
+*/
