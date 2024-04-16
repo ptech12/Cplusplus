13f172642fae2a8c6c49f617feffabc4a104c583
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-04-16 09:04:22 +0530

    hashing: find and return highest and lowest frequency elements

--- hashing/high_low.cpp
+++ hashing/high_low.cpp
@@ -1,11 +1,56 @@
-#include<iostream>
+#include "../bits/stdc++.h"
 using namespace std;
+
+vector<int> getFreq(vector<int> &v){
+    // pre-commpute
+    int max, min;
+
+    // mpp
+    unordered_map<int, int> mpp;
+    // result
+    vector<int> res;
+
+    for(int i=0; i < v.size(); i++){
+        mpp[v[i]]++;
+    }
+
+    for(auto it : mpp){
+        // cout << it.first << " -> " << it.second << endl;
+        // iterate through freq
+        if(max < it.second)
+            max = it.first; // assign key to max
+        
+        if(min > it.second)
+            min = it.first;
+    }
+
+    res.push_back(max);
+    res.push_back(min);
+
+    return res;
+}
 
 int main(int argc, char const *argv[])
 {
-    
-    // for loops
-    
+    vector<int> v;
+
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+    vector<int> ans = getFreq(v);
+
+    for(auto it : ans){
+        cout << it << " ";
+    }
+
+    // for(auto it : v){
+    //     cout << it << endl;
+    // }
+
     return 0;
 } 
 
