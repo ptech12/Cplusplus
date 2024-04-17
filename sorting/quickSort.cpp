11e721d35d564b2bb684f06c3e9c04370875a697
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-04-17 10:25:48 +0530

    sorting: end quick sort

--- sorting/quickSort.cpp
+++ sorting/quickSort.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 void printVector(vector<int> &arr);
+void swap(int *a, int *b);
+void quickSort(vector<int> &arr, int low, int high);
+int partition(vector<int> &arr, int low, int high);
 
 int main(){
     int n;
@@ -13,7 +16,7 @@ int main(){
         int temp;cin >> temp;
         arr.push_back(temp);
     }
-
+    quickSort(arr, 0, n-1);
     printVector(arr);
 
     return 0;
@@ -26,4 +29,56 @@ void printVector(vector<int> &arr)
         cout << it << " ";
 
     }
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void quickSort(vector<int> &arr, int low, int high)
+{
+    // run till  low crosses high
+    if(low < high){
+        // find partition
+        int partitionIndex = partition(arr, low, high);
+        // find left part of arr
+        quickSort(arr, low, partitionIndex - 1); 
+        quickSort(arr, partitionIndex + 1, high); 
+    }
+}
+
+
+/// @brief finds the partition index and places pivot in correct place for the given array
+/// @param arr Array of length N
+/// @param low starting index
+/// @param high ending index
+/// @return partition index of the pivot
+int partition(vector<int> &arr, int low, int high)
+{
+    // random pivot
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    // loop till i crosses j
+    while( i < j ){
+        // find all elements that are greater than pivot
+        while(arr[i] <= pivot && i <= high - 1){
+            i++;
+        }
+        // find all elements that are lesser than pivot
+        while(arr[j] > pivot && j >= low + 1){
+            j--;
+        }
+        // if i didn't cross j, swap them by reference
+        if(i < j)
+            swap(&arr[i], &arr[j]);
+    } 
+    // swap the pivot in correct order;
+    swap(&arr[low], &arr[j]);
+
+    return j; // the partition index
 }
